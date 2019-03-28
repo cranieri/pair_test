71bae65a8b8b6b18adf1189bb909a4594cdc1012
Cosimo Ranieri <cosimo.ranieri@currencycloud.com>
2019-03-28 10:24:46 +0000

    pair_test

--- gbp_amount.rb
+++
@@ -1,7 +0,0 @@
-require './amount'
-
-class GBPAmount < Amount
-  def fee
-    0.25 * amount/100
-  end
-end
--- usd_amount.rb
+++
@@ -1,7 +0,0 @@
-require './amount'
-
-class USDAmount < Amount
-  def fee
-    0.5 * amount/100
-  end
-end
