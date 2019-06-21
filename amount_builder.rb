243d94440626616e51cae75b184689d0cd59cce1
Cosimo Ranieri <cosimo.ranieri@currencycloud.com>
2019-06-21 09:57:10 +0100

    reset

--- amount_builder.rb
+++
@@ -1,6 +0,0 @@
-class AmountBuilder
-  def self.build(amount, currency)
-    amount_class = Kernel.const_get("#{currency}Amount")
-    amount_class.new(amount)
-  end
-end
--- eur_amount.rb
+++
@@ -1,7 +0,0 @@
-require './amount'
-
-class EURAmount < Amount
-  def fee
-    0.75 * amount/100
-  end
-end
